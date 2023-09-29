5c29428c0e30e23ec6c5fdc155dc074c1374f353
Saliim17 <ahouchsalim@gmail.com>
2023-09-29 21:45:45 +0200

    created flutter bloc auth architecture

---
+++ lib/bloc/auth_bloc.dart
@@ -0,0 +1,38 @@
+import 'package:bloc/bloc.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:my_be_real/states/auth_state.dart';
+
+import 'events/auth_event.dart';
+
+class AuthBloc extends Bloc<AuthEvent, AuthState> {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  AuthBloc() : super(AuthInitial());
+
+  Stream<AuthState> mapEventToState(AuthEvent event) async* {
+    if (event is SignInEvent) {
+      yield AuthLoading();
+
+      try {
+        final userCredential = await _firebaseAuth.signInWithEmailAndPassword(
+          email: event.email,
+          password: event.password,
+        );
+
+        final user = userCredential.user;
+        yield AuthAuthenticated(user!);
+      } catch (e) {
+        yield AuthError(e.toString());
+      }
+    } else if (event is SignOutEvent) {
+      yield AuthLoading();
+
+      try {
+        await _firebaseAuth.signOut();
+        yield AuthUnauthenticated();
+      } catch (e) {
+        yield AuthError(e.toString());
+      }
+    }
+  }
+}
---
+++ lib/bloc/events/auth_event.dart
@@ -0,0 +1,10 @@
+abstract class AuthEvent {}
+
+class SignInEvent extends AuthEvent {
+  final String email;
+  final String password;
+
+  SignInEvent({required this.email, required this.password});
+}
+
+class SignOutEvent extends AuthEvent {}
