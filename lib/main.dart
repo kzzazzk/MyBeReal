ebeada2a531c573a5d4c09e06102ffdd24b307e7
Saliim17 <ahouchsalim@gmail.com>
2023-09-29 22:28:39 +0200

    inject initial dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:my_be_real/dependencies/initial_dependencies.dart';
 import 'package:my_be_real/routes/routes.dart';
 
 String user = '';
@@ -9,9 +10,11 @@ String imgUrl = '';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+
   /*Reference storageRef = FirebaseStorage.instance.ref();
   Reference zakaLogoRef = storageRef.child("0/ZakaLogo.png");
   imgUrl = await zakaLogoRef.getDownloadURL();*/
+
   runApp(const MyApp());
 }
 
@@ -23,7 +26,7 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       getPages: RoutesClass.routes,
-      //initialBinding: BindingBuilder(() => Get.put(AuthController()));
+      initialBinding: InitialDependencies(),
       initialRoute: RoutesClass.getSplashScreen(),
       theme: ThemeData(
         fontFamily: GoogleFonts.lato().fontFamily,
