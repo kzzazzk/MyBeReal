e3411f3de8513466625f8980a5603fbff65f1d3f
kzzazzk <xcode@Mac-mini-de-Administrador-409.local>
2023-10-14 04:49:02 +0200

    Fixed ios push notifications

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,24 +1,27 @@
+
 import UIKit
 import Flutter
 import Firebase
+import FirebaseMessaging
+
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    didFinishLaunchingWithOptions launchOptions:
+ [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-      FirebaseApp.configure()
+    FirebaseApp.configure()
     GeneratedPluginRegistrant.register(with: self)
-      UNUserNotificationCenter.current().delegate = self
-
-      let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-      UNUserNotificationCenter.current().requestAuthorization(
-        options: authOptions,
-        completionHandler: { _, _ in }
-      )
-      application.registerForRemoteNotifications()
-
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    return super.application(application, didFinishLaunchingWithOptions:
+  launchOptions)
   }
-}
+  override func application(_ application: UIApplication,
+  didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
 
+   Messaging.messaging().apnsToken = deviceToken
+   print("Token: \(deviceToken)")
+   super.application(application,
+   didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
+ }
+}
